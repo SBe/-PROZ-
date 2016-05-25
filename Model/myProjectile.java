97aa565b72315fb160c3c564abc49af123d78545
SBe <szymonbezpalko@gmail.com>
2016-05-25 20:15:35 +0200

    Update myProjectile.java

--- Model/myProjectile.java
+++ Model/myProjectile.java
@@ -1,25 +1,42 @@
 package projectile;
 
+import java.util.ArrayList;
+
 import End.Screen;
 import End.Sprite;
+import Entity.Entity;
+import Entity.Monster;
 import Entity.ParticleSpawner;
-import Entity.Spawner;
+import Entity.Turrent;
 
 public class myProjectile extends Projectile{
-	public static final int  FIRE_RATE = 15;
-	public myProjectile(int x, int y, double dir){
+	public static final int  FIRE_RATE = 10;
+	public myProjectile(double x, double y, double dir){
 		super(x, y, dir);
-		range = 100 + random.nextInt(70);
-		damage = 20;
+		range = 200 + random.nextInt(70);
+		damage = 10;
 		sprite = Sprite.projectile;
-		speed = 3;
+		speed = 5;
 		nx = speed * Math.cos(dir) +  random.nextDouble() - 0.5;
 		ny = speed * Math.sin(dir) +  random.nextDouble() - 0.5;
 	}
 	public void update(){
+		//List<Entity> monster_list = level.getEntities();
+	//	if()
+		ArrayList<Entity> asd = (ArrayList<Entity>) level.getEntities();
+		for(int i = 0 ; i < asd.size(); i++	)
+			if((asd.get(i) instanceof Turrent || asd.get(i) instanceof Monster)&& ((int)(x + nx) >= asd.get(i).getX())  && ((int)(x + nx) <= asd.get(i).getX() + 16) 
+					&& (int)(y + ny) >= asd.get(i).getY() - 8 && (int)(y + ny) <= asd.get(i).getY() + 8){
+				remove();
+				asd.get(i).setHP(asd.get(i).getHP() - damage);
+				if(asd.get(i).getHP() == 0){
+					asd.get(i).remove();
+				}
+				level.add(new ParticleSpawner((int)(x - 8), (int)(y - 8), 30, 1, level));
+			}
 		if(level.tileCollision((int)(x + nx), (int)(y + ny),  5,  5, 6)){
 				remove();
-				level.add(new ParticleSpawner((int)(x - 8), (int)(y - 8), 30, 50, level));
+				level.add(new ParticleSpawner((int)(x - 8), (int)(y - 8), 30, 5, level));
 		}
 			move();
 	}
@@ -31,6 +48,7 @@ public class myProjectile extends Projectile{
 			this.x += nx;
 			this.y += ny;
 		}
+	
 		if(calculate() > range){
 			remove();
 		}
